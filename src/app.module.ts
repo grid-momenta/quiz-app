26f6583446cdff60a63021f6c786835cbfc91d10
DHN Chandan <php.chandan@gmail.com>
2023-02-22 22:31:33 +0600

    feat: :recycle: Add quiz, question and answer resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AnswersModule } from './answers/answers.module';
+import { QuestionsModule } from './questions/questions.module';
+import { QuizzesModule } from './quizzes/quizzes.module';
 
 @Module({
-  imports: [],
+  imports: [AnswersModule, QuestionsModule, QuizzesModule],
   controllers: [AppController],
   providers: [AppService],
 })
